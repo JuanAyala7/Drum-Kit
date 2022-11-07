dcfd07de38bf1d53441c9890d8792f730cabb3a4
Juan Luis Ayala Jr <113149815+JuanAyala7@users.noreply.github.com>
2022-11-07 17:36:19 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 
-document.querySelector("button").addEventListener("click", handleClick);
+document.querySelector("button").addEventListener("click", handleClick); //the event listener is what will be waiting to listen to what is selected (button)
 
-function handleClick() {
+function handleClick() { //this function is the action (click) the event listener is waiting to hear before sedning the alert
   alert("I got Clicked!");
 }
