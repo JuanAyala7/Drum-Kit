52f9276cbf6299e043515a070b56aa54cfdd91cd
Juan Luis Ayala Jr <113149815+JuanAyala7@users.noreply.github.com>
2022-11-07 17:43:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 
 document.querySelector("button").addEventListener("click", handleClick); //the event listener is what will be waiting to listen to what is selected (button)
 
-function handleClick() { //this function is the action (click) the event listener is waiting to hear before sedning the alert
-  alert("I got Clicked!");
+function handleClick() { //this function is the action (click) the event listener is waiting to hear before sending the alert
+  alert("I got Clicked!"); // it's the idea of passing a function as an input so that it can be called at a later time.
 }
