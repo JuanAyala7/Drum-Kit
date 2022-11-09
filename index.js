b24a598b1cd9ef1828b4984777e363d69db715d4
Juan Luis Ayala Jr <113149815+JuanAyala7@users.noreply.github.com>
2022-11-08 18:02:19 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ for (var i = 0; i<numberOfDrumButtons; i++) {
 
   document.querySelectorAll(".drum") [i].addEventListener ("click", function () {
     
-      console.log(this.innerHTML);
+      this.style.color = "white";
     
   });
 
