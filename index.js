144b8041fc8ec870c091266a795a9f10f6304cc2
Juan Luis Ayala Jr <113149815+JuanAyala7@users.noreply.github.com>
2022-11-11 18:20:55 -0600

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ for (var i = 0; i<numberOfDrumButtons; i++) {
       var buttonInnerHTML = this.innerHTML;
 
   switch (buttonInnerHTML) {
-    case "w": 
-        var tom1 = new Audio('sounds/tom-1.mp3');
-        tom1.play();
-      break;
+     case "w": 
+       var tom1 = new Audio('sounds/tom-1.mp3');
+       tom1.play();
+       break;
 
      case "a":
        var tom2 = new Audio('sounds/tom-2.mp3');
@@ -26,6 +26,21 @@ for (var i = 0; i<numberOfDrumButtons; i++) {
      case "d": 
        var tom4 = new Audio('sounds/tom-4.mp3');
        tom4.play();
+       break;
+      
+     case "j":
+       var snare = new Audio('sounds/snare.mp3');
+       snare.play();
+       break;
+
+     case "k":
+       var crash = new Audio('sounds/crash.mp3');
+       crash.play();
+       break;
+
+     case "l":
+       var kick = new Audio('sounds/kick-bass.mp3');
+       kick.play();
        break;
 
       
