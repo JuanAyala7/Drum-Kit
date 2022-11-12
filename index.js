1772630902b4713bc4fc02897619e2da761f0d00
Juan Luis Ayala Jr <113149815+JuanAyala7@users.noreply.github.com>
2022-11-11 18:14:34 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,25 @@ for (var i = 0; i<numberOfDrumButtons; i++) {
 
   document.querySelectorAll(".drum") [i].addEventListener ("click", function () {
     
-      this.style.color = "white";
+      var buttonInnerHTML = this.innerHTML;
+
+  switch (buttonInnerHTML) {
+    case "w": 
+        var tom1 = new Audio('sounds/tom-1.mp3');
+        tom1.play();
+      break;
+
+     case "a":
+       var tom2 = new Audio('sounds/tom-2.mp3');
+       tom2.play();
+       break;
+
+
+      
+
+    default:
+      
+  }
     
   });
 
